0d1bafe1e2bb57f264c459b30172324f0014c595
Ivan Buzyka <ibu@sitecore.net>
2014-07-28 16:22:51 +0200

    Changed Deploy script to be called from PSake.

---
+++ Buildscript/DeployEntryPoint.ps1
@@ -0,0 +1,7 @@
+﻿Clear-Host
+
+remove-module [p]sake
+import-module .\Tools\psake\psake.psm1
+
+$psake.use_exit_on_error = $true 
+Invoke-psake .\deploy.ps1 Deploy -parameters @{ buildNumber = '12345'; }
--- Buildscript/deploy.ps1
+++ Buildscript/deploy.ps1
@@ -11,44 +11,53 @@ Import-Module IISUtils
 Import-Module FileUtils
 
 # Main variables
-$buildFolder = Resolve-Path .. 
-$siteName = "LaunchSitecore7"
-$licensePath = "C:\ibu\license\license.xml"
-$sourcePath = "$buildFolder\Output\LaunchSitecore.Build.12345.zip"
-$targetFolder = "C:\inetpub\wwwroot\LaunchSitecore7"
-$sqlServerName = "$env:COMPUTERNAME\SQLEXPRESS"
+properties {
+    $buildFolder = Resolve-Path .. 
+    $siteName = "LaunchSitecore7"
+    $licensePath = "C:\ibu\license\license.xml"
+    $sourcePath = "$buildFolder\Output\LaunchSitecore.Build.$buildNumber.zip"
+    $targetFolder = "C:\inetpub\wwwroot\LaunchSitecore7"
+    $sqlServerName = "$env:COMPUTERNAME\SQLEXPRESS"
 
-$server = New-Object ("Microsoft.SqlServer.Management.Smo.Server") $sqlServerName
-$databases = "core", "master", "web", "analytics"
-
-# Cleanup Databases from previuous installation (if needed)
-foreach ($db in $databases)
-{
-    Delete-Database $server "$siteName.$db"
+    $server = New-Object ("Microsoft.SqlServer.Management.Smo.Server") $sqlServerName
+    $databases = "core", "master", "web", "analytics"
 }
 
-New-Item $targetFolder -type directory -Force -Verbose
+task Deploy -depends CleanupDB, PerformDeploy
 
-# Additional variables
-$packageFileName = [System.IO.Path]::GetFileNameWithoutExtension($sourcePath)
-$dataFolder = "$targetFolder\Data"
-$websiteFolder = "$targetFolder\Website"
-
-# Main Script
-Unzip-Archive $sourcePath $targetFolder
-
-# Attach Databases
-foreach ($db in $databases)
-{
-    Attach-Database $server "$siteName.$db" "$targetFolder\Databases\Sitecore.$db.mdf" "$targetFolder\Databases\Sitecore.$db.ldf"
-	Set-ConnectionString "$websiteFolder\App_Config\ConnectionStrings.config" "$db" "Trusted_Connection=Yes;Data Source=$sqlServerName;Database=$siteName.$db"
+task CleanupDB {
+    # Cleanup Databases from previuous installation (if needed)
+    foreach ($db in $databases)
+    {
+        Delete-Database $server "$siteName.$db"
+    }    
 }
 
-Set-ConfigAttribute "$websiteFolder\web.config" "sitecore/sc.variable[@name='dataFolder']" "value" $dataFolder   
+task PerformDeploy {
+    New-Item $targetFolder -type directory -Force -Verbose
+    # Additional variables
+    $packageFileName = [System.IO.Path]::GetFileNameWithoutExtension($sourcePath)
+    $dataFolder = "$targetFolder\Data"
+    $websiteFolder = "$targetFolder\Website"
 
-Copy-Item $licensePath $dataFolder
-Create-AppPool $siteName "v4.0"
-Create-Site $siteName "$siteName.local" "$targetFolder"
-Add-HostFileContent "127.0.0.1" "$siteName.local"
-Get-WebPage "$siteName.local/InstallPackages.aspx"
-Get-WebPage "$siteName.local/Publish.aspx"
+
+    # Main Script
+    Unzip-Archive $sourcePath $targetFolder
+
+    # Attach Databases
+    foreach ($db in $databases)
+    {
+        Attach-Database $server "$siteName.$db" "$targetFolder\Databases\Sitecore.$db.mdf" "$targetFolder\Databases\Sitecore.$db.ldf"
+	    Set-ConnectionString "$websiteFolder\App_Config\ConnectionStrings.config" "$db" "Trusted_Connection=Yes;Data Source=$sqlServerName;Database=$siteName.$db"
+    }
+    Set-ConfigAttribute "$websiteFolder\web.config" "sitecore/sc.variable[@name='dataFolder']" "value" $dataFolder   
+
+    Copy-Item $licensePath $dataFolder
+    Create-AppPool $siteName "v4.0"
+    Create-Site $siteName "$siteName.local" "$targetFolder"
+    Add-HostFileContent "127.0.0.1" "$siteName.local"
+    Get-WebPage "$siteName.local/InstallPackages.aspx"
+    Get-WebPage "$siteName.local/Publish.aspx"
+}
+
+
